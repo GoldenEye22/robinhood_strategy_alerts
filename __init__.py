ea8e1233d91e4cb9bc1f07f207f9ed1ff299bc5f
GoldenEye22 <38599927+GoldenEye22@users.noreply.github.com>
2019-06-08 20:52:19 -0400

    not needed

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 22 23:44:33 2018
-
-@author: Andy
-"""
-#Import code
-import robinhood_strategy_alerts.robinhood_strategy_alerts as robinhood_strategy_alerts
