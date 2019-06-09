9b81bfc8efc7c130c2ef111c437a6d50713dff8a
GoldenEye22 <GoldenEye22@github.com>
2019-06-08 20:25:30 -0400

    executable file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 22 23:44:33 2018
+
+@author: Andy
+"""
+#Import code
+import robinhood_strategy_alerts.robinhood_strategy_alerts as robinhood_strategy_alerts
